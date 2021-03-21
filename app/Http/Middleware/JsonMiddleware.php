0260c4c14720a868b71fff1c27f06120218f88ce
rabbanee <daffarabbanee@gmail.com>
2021-03-21 22:31:25 +0700

    tes json middleware

--- app/Http/Middleware/JsonMiddleware.php
+++ app/Http/Middleware/JsonMiddleware.php
@@ -17,6 +17,7 @@ class JsonMiddleware
     public function handle(Request $request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
+        $request->headers->set('Access-Control-Allow-Origin', '*');
         return $next($request);
     }
 }
